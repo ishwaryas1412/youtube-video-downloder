d89111822fc8d46f7295f8a038e8b2a7b4382e49
Ishwarya Sekar <52875849+ishwaryas1412@users.noreply.github.com>
2020-09-17 11:09:24 +0530

    commit

--- utube.py
+++ utube.py
@@ -1,6 +1,6 @@
 from pytube import YouTube	#module for downloading youtube video
 from tkinter import *		#module making Graphical User Interface
-import tkinter as tk
+import tkinter as tk #import tkinter
 import tkinter.messagebox
 import time #pip install datetime
 
